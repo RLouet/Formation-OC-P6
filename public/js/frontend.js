16fad8b5005dfac2189d88cfb23ee4568a61e42e
LouetR <romainlouet@romsworld.net>
2021-04-29 19:53:10 +0200

    fix codacy issues

--- public/js/frontend.js
+++ public/js/frontend.js
@@ -1,6 +1,22 @@
 $(document).ready(function() {
     const $LOGIN_MODAL = $("#loginModal");
 
+    function removeHashGet(value) {
+        const uri = window.location.toString();
+        if (uri.indexOf(value) > 0) {
+            const CLEAN_URI = uri.substring(
+                0,
+                uri.indexOf(value)
+            );
+
+            window.history.replaceState(
+                {},
+                document.title,
+                CLEAN_URI
+            );
+        }
+    }
+
     if (location.hash !== "#login") {
         removeHashGet("?target=");
     }
@@ -17,26 +33,11 @@ $(document).ready(function() {
         }
     };
 
-    $LOGIN_MODAL.on('hide.bs.modal', function(e){
+    $LOGIN_MODAL.on("hide.bs.modal", function(e){
         removeHashGet("?target=");
         if (location.hash === "login") {
             removeHashGet("#");
         }
     });
 
-    function removeHashGet(value) {
-        const uri = window.location.toString();
-        if (uri.indexOf(value) > 0) {
-            const CLEAN_URI = uri.substring(
-                0,
-                uri.indexOf(value)
-            );
-
-            window.history.replaceState(
-                {},
-                document.title,
-                CLEAN_URI
-            );
-        }
-    }
 });
