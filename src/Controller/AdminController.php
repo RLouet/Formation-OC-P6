273b078e85586485ac88eb9e79fa2b52ed321df2
LouetR <romainlouet@romsworld.net>
2021-06-05 20:21:34 +0200

    Create Users Management page

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -4,6 +4,7 @@
 namespace App\Controller;
 
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,8 +14,16 @@ class AdminController extends AbstractController
     /**
      * @Route("/admin", name="admin_users")
      */
-    public function admin(): Response
+    public function admin(UserRepository $userRepository): Response
     {
-        return new Response('Admin');
+
+        $users = $userRepository->findBy(
+            [],
+            ['id' => 'ASC'],
+            $this->getParameter('app.tricks_pagination_length')
+        );
+        return $this->render('admin/users-manage.html.twig', [
+            'users' => $users,
+        ]);
     }
 }
